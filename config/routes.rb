4bde980bc7b53c058154ac9516964ec361a113bc
Rae <info@rae-farine.net>
2014-07-14 20:08:23 -0400

    Added Bookmarks

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  
+  root 'site#index'
+
+  devise_for :users
+
+  resources :bookmarks
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
